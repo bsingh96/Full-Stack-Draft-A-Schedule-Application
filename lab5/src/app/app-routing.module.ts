1ff2365e0c052a82b988c7b92dbcec6b374c8046
Bikramjit Singh <bsingh96@uwo.ca>
2020-11-23 20:11:05 -0500

    added login page and a homepage.

--- lab5/src/app/app-routing.module.ts
+++ lab5/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ViewpageComponent} from './components/viewpage/viewpage.component'
+import { LoginComponent} from './components/login/login.component'
+const routes : Routes = [
+  {path : 'viewpage' , component: ViewpageComponent},
+  {path : 'login' , component: LoginComponent},
+  {path: '', redirectTo:'viewpage', pathMatch:'full'}
+  
+];
 
-const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [ViewpageComponent,LoginComponent]
--- lab5/src/app/app.module.ts
+++ lab5/src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewpageComponent } from './components/viewpage/viewpage.component';
+import { LoginComponent } from './components/login/login.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewpageComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
